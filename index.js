afb9df4c313af1de9d60a24b6e2f345b60a7801f
Darshan Tailor <darshanhtailor@gmail.com>
2021-12-30 18:19:00 +0530

    initial commit 2

--- index.js
+++ index.js
@@ -16,35 +16,35 @@ $('#themes').change(()=>{
 });
 
 // Executing code
-var request = require('request');
+// var request = require('request');
 
-var program = {
-    script : `
-    #include <bits/stdc++.h>
-    using namespace std;
-    typedef long long int ll;
+// var program = {
+//     script : `
+//     #include <bits/stdc++.h>
+//     using namespace std;
+//     typedef long long int ll;
 
-    int main(){
-        int n;
-        cin >> n;
-        cout << "Hello World! " << n;
-        return 0;
-    }
-    `,
-    // stdin: ,
-    language: "cpp17",
-    versionIndex: "0",
-    clientId: "d21cb78a335b3bfc69f727ba0fc7df75",
-    clientSecret:"4dba8c36e4bd0ac5bee247b30454ffd1b88b8ea3830b9870c6f196137d2ecda2"
-};
-request({
-    url: 'https://api.jdoodle.com/v1/execute',
-    method: "POST",
-    json: program
-},
-function (error, response) {
-    if(error)
-        console.log('Unable to reach the server. Please check your network.')
-    else
-        console.log(response.body.output);
-});
+//     int main(){
+//         int n;
+//         cin >> n;
+//         cout << "Hello World! " << n;
+//         return 0;
+//     }
+//     `,
+//     // stdin: ,
+//     language: "cpp17",
+//     versionIndex: "0",
+//     clientId: "d21cb78a335b3bfc69f727ba0fc7df75",
+//     clientSecret:"4dba8c36e4bd0ac5bee247b30454ffd1b88b8ea3830b9870c6f196137d2ecda2"
+// };
+// request({
+//     url: 'https://api.jdoodle.com/v1/execute',
+//     method: "POST",
+//     json: program
+// },
+// function (error, response) {
+//     if(error)
+//         console.log('Unable to reach the server. Please check your network.')
+//     else
+//         console.log(response.body.output);
+// });
